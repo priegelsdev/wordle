0bd558b94498bfb2382b342e59dfe70fa8712178
priegelsdev <priegelsdev@gmail.com>
2023-02-15 17:29:25 +0100

    create boxes for letters

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,23 @@ export default function App() {
       </header>
 
       <div className="m-auto lg text-center">
-        <div>Word_</div>
+        <div className="flex gap-1">
+          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
+            W
+          </div>
+          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
+            O
+          </div>
+          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
+            R
+          </div>
+          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
+            D
+          </div>
+          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
+            0
+          </div>
+        </div>
         <div>Word1</div>
         <div>Word2</div>
         <div>Word3</div>
