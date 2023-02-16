b01216557a58a222c003a768f7f1c5e512286c9f
priegelsdev <priegelsdev@gmail.com>
2023-02-16 10:07:39 +0100

    add svgs for backspace and return to keyboard

--- src/Components/Keyboard.tsx
+++ src/Components/Keyboard.tsx
@@ -1,3 +1,37 @@
+const arrow = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    className="arrow m-auto w-6 h-6"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z"
+    />
+  </svg>
+);
+
+const enter = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="enter w-6 h-6 m-auto"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4.5 12.75l6 6 9-13.5"
+    />
+  </svg>
+);
+
 const KEYS = [
   'a',
   'b',
@@ -25,6 +59,8 @@ const KEYS = [
   'x',
   'y',
   'z',
+  arrow,
+  enter,
 ];
 
 const keyElements = KEYS.map((key) => (
