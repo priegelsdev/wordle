48f525ac2901bbee5fd79987f482ac095ffe891a
priegelsdev <priegelsdev@gmail.com>
2023-02-16 11:36:47 +0100

    change styling slightly

--- src/App.tsx
+++ src/App.tsx
@@ -26,40 +26,15 @@ export default function App() {
         </button>
       </header>
 
-      <div className="m-auto lg text-center">
+      <div className="m-auto mb-0 lg text-center">
         <RowsContainer quizWord={quizWord} />
       </div>
-
-      {/* <div className="m-auto lg text-center">
-        <div className="flex gap-1">
-          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
-            W
-          </div>
-          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
-            O
-          </div>
-          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
-            R
-          </div>
-          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
-            D
-          </div>
-          <div className="px-4 py-3 border-solid border-2 flex justify-center items-center">
-            0
-          </div>
-        </div>
-        <div>Word1</div>
-        <div>Word2</div>
-        <div>Word3</div>
-        <div>Word4</div>
-        <div>Word5</div>
-      </div> */}
 
       <div className="m-auto">
         <Keyboard />
       </div>
 
-      <footer className="p-5 text-center bg-gray-600 text-white">info</footer>
+      <footer className="p-5 text-center bg-gray-600 text-white">footer</footer>
     </main>
   );
 }
