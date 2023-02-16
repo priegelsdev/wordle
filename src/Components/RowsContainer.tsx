0278813240f4ef2183a4153db89d6875b0fbdbbc
priegelsdev <priegelsdev@gmail.com>
2023-02-16 11:31:03 +0100

    make all rows show up

--- src/Components/RowsContainer.tsx
+++ src/Components/RowsContainer.tsx
@@ -4,30 +4,27 @@ type RowsContainerProps = {
 
 export default function RowsContainer({ quizWord }: RowsContainerProps) {
   const wordArr = quizWord.split('');
-  console.log(wordArr);
+
+  // letters mapped out and rendered as boxes wihtout any value; value is stored in the id to later on..
+  // ..see if value entered matches id value
+  // likely needed to think through again as logic gets added to compare entered value and display diff colors
+
+  const letterElements = wordArr.map((letter, index) => (
+    <div
+      key={index}
+      id={letter}
+      className="aspect-square w-14 bg-asphalt-gray text-white font-bold flex justify-center items-center"
+    ></div>
+  ));
 
   return (
     <>
-      <div className="flex gap-1">
-        <div className="aspect-square w-14 bg-asphalt-gray text-white font-bold flex justify-center items-center"></div>
-        <div className="aspect-square w-14 bg-asphalt-gray text-white font-bold flex justify-center items-center">
-          O
-        </div>
-        <div className="aspect-square w-14 bg-asphalt-gray text-white font-bold flex justify-center items-center">
-          R
-        </div>
-        <div className="aspect-square w-14 bg-asphalt-gray text-white font-bold flex justify-center items-center">
-          D
-        </div>
-        <div className="aspect-square w-14 bg-asphalt-gray text-white font-bold flex justify-center items-center">
-          0
-        </div>
-      </div>
-      <div>Word1</div>
-      <div>Word2</div>
-      <div>Word3</div>
-      <div>Word4</div>
-      <div>Word5</div>
+      <div className="flex gap-1 mb-1">{letterElements}</div>
+      <div className="flex gap-1 mb-1">{letterElements}</div>
+      <div className="flex gap-1 mb-1">{letterElements}</div>
+      <div className="flex gap-1 mb-1">{letterElements}</div>
+      <div className="flex gap-1 mb-1">{letterElements}</div>
+      <div className="flex gap-1 mb-1">{letterElements}</div>
     </>
   );
 }
