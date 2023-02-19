31470272cf71bf0346f22d6d5443c697578fb1c3
priegelsdev <priegelsdev@gmail.com>
2023-02-19 19:50:03 +0100

    change header styling slightly

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,3 +1,20 @@
+const copy = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+    />
+  </svg>
+);
+
 const de = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -155,9 +172,12 @@ export default function Header({ englishMode, onClick }: HeaderProps) {
   return (
     <header className="flex items-center justify-between p-2 sm:p-6 border-solid border-b-2 shadow-lg">
       <div /* div exists only for flex to work properly */></div>
-      <h1 className="text-4xl tracking-tighter font-bold font-sans justify-self-start mx-2 mr-auto sm:-mr-12 sm:ml-24">
-        Wordle
-      </h1>
+      <div className="flex items-center mr-auto ml-1 sm:-mr-32">
+        {copy}
+        <h1 className="text-4xl tracking-tighter font-bold font-sans justify-self-start ml-1">
+          Wordle
+        </h1>
+      </div>
       <div className="flex items-center gap-3 mx-2">
         <button onClick={() => location.reload()}>{restart}</button>
         <button
